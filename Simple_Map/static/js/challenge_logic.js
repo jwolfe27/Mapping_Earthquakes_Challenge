6baf9781f1052919be97575ce08be577638a46b8
Jake Wolfe <89044350+jwolfe27@users.noreply.github.com>
2021-11-19 10:24:38 -0500

    Add files via upload

--- Simple_Map/static/js/challenge_logic.js
+++ Simple_Map/static/js/challenge_logic.js
@@ -15,7 +15,12 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
 	accessToken: API_KEY
 });
 
-
+// We create the third tile layer that will be the background of our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+	maxZoom: 18,
+	accessToken: API_KEY
+});
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
@@ -26,9 +31,9 @@ let map = L.map('mapid', {
 
 // Create a base layer that holds all three maps.
 let baseMaps = {
-  Streets: streets,
-  Satellite: satelliteStreets
-  
+  "Streets": streets,
+  "Satellite": satelliteStreets,
+  "Dark": dark
 };
 
 // 1. Add a 2nd layer group for the tectonic plate data.
